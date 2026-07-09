527cde8f7b12646d7ae0f95b74f5af530737fcdc
hddq <hddqgit@gmail.com>
2026-07-09 21:30:17 +0200

    feat(cloudflare): add simple email routing

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -31,6 +31,7 @@ output "zone_id" {
 
 locals {
   zone_id = data.cloudflare_zones.my_domain.result[0].id
+  account_id = data.sops_file.secrets.data["cloudflare_account_id"]
 }
 
 # Production: *.lab.hddq.org → 192.168.41.10 (Traefik)
@@ -79,4 +80,78 @@ resource "cloudflare_dns_record" "frc_github_pages" {
   content = "hddq.github.io"
   ttl     = 1
   proxied = false
+}
+
+# Email Routing: hddq.org
+resource "cloudflare_dns_record" "mx1" {
+  zone_id  = local.zone_id
+  name     = "hddq.org"
+  type     = "MX"
+  content  = "route1.mx.cloudflare.net"
+  priority = 14
+  ttl      = 1
+}
+
+resource "cloudflare_dns_record" "mx2" {
+  zone_id  = local.zone_id
+  name     = "hddq.org"
+  type     = "MX"
+  content  = "route2.mx.cloudflare.net"
+  priority = 28
+  ttl      = 1
+}
+
+resource "cloudflare_dns_record" "mx3" {
+  zone_id  = local.zone_id
+  name     = "hddq.org"
+  type     = "MX"
+  content  = "route3.mx.cloudflare.net"
+  priority = 7
+  ttl      = 1
+}
+
+resource "cloudflare_dns_record" "spf" {
+  zone_id = local.zone_id
+  name    = "hddq.org"
+  type    = "TXT"
+  content = "v=spf1 include:_spf.mx.cloudflare.net ~all"
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "dkim" {
+  zone_id = local.zone_id
+  name    = "cf2024-1._domainkey.hddq.org"
+  type    = "TXT"
+  content = "v=DKIM1; h=sha256; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiweykoi+o48IOGuP7GR3X0MOExCUDY/BCRHoWBnh3rChl7WhdyCxW3jgq1daEjPPqoi7sJvdg5hEQVsgVRQP4DcnQDVjGMbASQtrY4WmB1VebF+RPJB2ECPsEDTpeiI5ZyUAwJaVX7r6bznU67g7LvFq35yIo4sdlmtZGV+i0H4cpYH9+3JJ78km4KXwaf9xUJCWF6nxeD+qG6Fyruw1Qlbds2r85U9dkNDVAS3gioCvELryh1TxKGiVTkg4wqHTyHfWsp7KD3WQHYJn0RyfJJu6YEmL77zonn7p2SRMvTMP3ZEXibnC9gz3nnhR6wcYL8Q7zXypKTMD58bTixDSJwIDAQAB"
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "dmarc" {
+  zone_id = local.zone_id
+  name    = "_dmarc"
+  type    = "TXT"
+  content = "v=DMARC1; p=reject;"
+  ttl     = 1
+}
+
+resource "cloudflare_email_routing_address" "my_gmail" {
+  account_id = local.account_id
+  email      = "hddqgit@gmail.com"
+}
+
+resource "cloudflare_email_routing_rule" "git_forward" {
+  zone_id = local.zone_id
+  name    = "Redirect Git"
+  enabled = true
+
+  matchers = [{
+    type  = "literal"
+    field = "to"
+    value = "git@hddq.org"
+  }]
+
+  actions = [{
+    type  = "forward"
+    value = [cloudflare_email_routing_address.my_gmail.email]
+  }]
 }
