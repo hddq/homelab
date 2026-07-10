2e5d49c88b0250480c318d316803521cc0fed5d7
hddq <git@hddq.org>
2026-07-10 20:08:38 +0200

    feat(nix): add NixOS configuration and disk setup for Oracle Cloud vps0

---
+++ nix/vps0/configuration.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, modulesPath, ... }:
+
+{
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.availableKernelModules = [ "xhci_pci" "usbhid" "virtio_pci" "virtio_blk" ];
+
+  networking.hostName = "vps0";
+  networking.useDHCP = true;
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ 22 ];
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
+
+  programs.ssh.startAgent = true;
+  users.users.hddq = {
+    isNormalUser = true;
+    description = "hddq";
+    extraGroups = [ "networkmanager" "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKFgv0ykKB0lLGjkh3fI8tUy+o8qtUcgjFPSN1AyncW hddq@main"
+    ];
+  };
+  security.sudo.wheelNeedsPassword = false;
+
+  system.stateVersion = "26.05";
+}
---
+++ nix/vps0/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "NixOS configuration for Oracle Cloud vps0";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, disko, ... }@inputs: {
+    nixosConfigurations."vps0" = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        disko.nixosModules.disko
+        ./disk-config.nix
+        ./configuration.nix
+      ];
+    };
+  };
+}
