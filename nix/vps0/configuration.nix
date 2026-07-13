aa89819fcffc1ed741f0f142ac5136130168da6d
hddq <git@hddq.org>
2026-07-13 02:10:09 +0200

    fix: lint

--- nix/vps0/configuration.nix
+++ nix/vps0/configuration.nix
@@ -1,4 +1,8 @@
-{config, modulesPath, ...}: {
+{
+  config,
+  modulesPath,
+  ...
+}: {
   imports = [(modulesPath + "/profiles/qemu-guest.nix")];
 
   boot = {
@@ -18,7 +22,7 @@
     firewall.allowedUDPPorts = [51820];
     wireguard.interfaces = {
       wg0 = {
-        ips = [ "192.168.70.2/24" ];
+        ips = ["192.168.70.2/24"];
         listenPort = 51820;
         privateKeyFile = config.sops.secrets.wireguard_private_key.path;
 
@@ -26,12 +30,12 @@
           {
             # openwrt
             publicKey = "TCiRCR6qUTA/DiyCFsBFDJ+t5U2MfUf+ilJiFWrJe24=";
-            allowedIPs = [ "192.168.70.1/32" "192.168.40.0/24" "192.168.41.0/24" "192.168.10.0/24" ];
+            allowedIPs = ["192.168.70.1/32" "192.168.40.0/24" "192.168.41.0/24" "192.168.10.0/24"];
           }
           {
             # tundra
             publicKey = "kJWA/gR6RTYrJm+XMC1DDLuTq079SvVZE5QDST/qATc=";
-            allowedIPs = [ "192.168.70.15/32" ];
+            allowedIPs = ["192.168.70.15/32"];
           }
         ];
       };
