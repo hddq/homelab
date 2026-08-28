7098b213d74286606c9ce306b6de9e74cd7ba69d
hddq <git@hddq.org>
2026-08-28 18:22:58 +0200

    fix(unbound): fix remote-control configuration syntax for nix and k8s

--- nix/vps0/unbound.nix
+++ nix/vps0/unbound.nix
@@ -6,12 +6,21 @@ _: {
       server = {
         include = ["${../../shared/dns/unbound.conf}"];
       };
+      remote-control = {
+        control-enable = true;
+        control-use-cert = false;
+      };
     };
   };
 
   services.prometheus.exporters.unbound = {
     enable = true;
     openFirewall = true;
+    unbound = {
+      ca = null;
+      certificate = null;
+      key = null;
+    };
   };
 
   networking.firewall.allowedTCPPorts = [5353 9167];
